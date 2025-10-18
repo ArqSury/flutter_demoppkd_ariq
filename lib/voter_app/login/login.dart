8f737535eefbed6ba29cb1d273b74fcf7f8a02de
ArqSury <Ariqsurya0510@gmail.com>
2025-10-18 14:29:13 +0700

    new

--- lib/voter_app/login/login.dart
+++ lib/voter_app/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demoppkd_ariq/day_13/reuse/input_user.dart';
 
 class LoginWidget extends StatefulWidget {
   const LoginWidget({super.key});
@@ -11,26 +12,44 @@ class _LoginWidgetState extends State<LoginWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
-          alignment: AlignmentDirectional.center,
-          children: [
-            Container(color: Colors.red, height: 840, width: 500),
-            Positioned(
-              bottom: 40,
-              child: Container(
-                height: 700,
-                width: 340,
-                alignment: Alignment.center,
+      body: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          Container(color: Colors.red, height: 840, width: 500),
+          Stack(
+            clipBehavior: Clip.antiAlias,
+            alignment: AlignmentDirectional.topStart,
+            children: [
+              Container(
+                height: 680,
+                width: double.infinity,
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
                   boxShadow: [BoxShadow(blurRadius: 8, color: Colors.black)],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [SizedBox(height: 120), Text("Nama:")],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: -88,
+            child: Container(
+              margin: EdgeInsets.all(8),
+              child: Image(
+                image: AssetImage(
+                  "assets/images/voter_app/logo_voterson_nobg2.png",
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
