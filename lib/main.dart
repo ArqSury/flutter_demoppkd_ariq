1c442771423be9948616db0d09c95f2b026a35ad
ArqSury <Ariqsurya0510@gmail.com>
2025-10-21 13:04:36 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_demoppkd_ariq/day_16/tugas7_flutter.dart';
 
-void main() {
+void main() async {
+  await initializeDateFormatting("id_ID", null);
   runApp(const MyApp());
 }
 
