985786d37413bab293d41ba6cbadeb094b9ced27
ArqSury <Ariqsurya0510@gmail.com>
2025-10-21 11:17:22 +0700

    commit

--- lib/day_16/drawer/kategori_produk.dart
+++ lib/day_16/drawer/kategori_produk.dart
@@ -8,8 +8,48 @@ class ProdukKategori extends StatefulWidget {
 }
 
 class _ProdukKategoriState extends State<ProdukKategori> {
+  String? dropDownValue;
+  final List<String> listProduct = [
+    "Elektronik",
+    "Makanan",
+    "Otomotif",
+    "Sastra",
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.all(8)),
+          Text(
+            "Kategori Produk",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            spacing: 8,
+            children: [
+              Padding(padding: EdgeInsets.all(8)),
+              Text("Pilih produk:"),
+              DropdownButton(
+                value: dropDownValue,
+                items: listProduct.map((String val) {
+                  return DropdownMenuItem(
+                    value: val,
+                    child: Text(val, style: TextStyle(color: Colors.black)),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    dropDownValue = value;
+                  });
+                },
+              ),
+            ],
+          ),
+          Divider(),
+          Expanded(child: Column(children: [])),
+        ],
+      ),
+    );
   }
 }
