e9a0bd0d94081d35a808eba2b84bcc7d4a667924
ArqSury <Ariqsurya0510@gmail.com>
2025-10-22 10:11:05 +0700

    commit

---
+++ lib/voter_app/branches/home_branch.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeBranch extends StatefulWidget {
+  const HomeBranch({super.key});
+
+  @override
+  State<HomeBranch> createState() => _HomeBranchState();
+}
+
+class _HomeBranchState extends State<HomeBranch> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/voter_app/functions/drawerinput.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Drawerinput extends StatefulWidget {
+  const Drawerinput({super.key});
+
+  @override
+  State<Drawerinput> createState() => _DrawerinputState();
+}
+
+class _DrawerinputState extends State<Drawerinput> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer();
+  }
+}
